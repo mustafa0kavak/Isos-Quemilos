6fb18cf7615c412ff12eee395e034eb7bc29d87e
Mustafa Kavak <101901319+mustafa0kavak@users.noreply.github.com>
2024-05-25 17:15:06 +0300

    Update IsosQuemilos.cpp

--- IsosQuemilos/IsosQuemilos.cpp
+++ IsosQuemilos/IsosQuemilos.cpp
@@ -1,11 +1,46 @@
-﻿#include <iostream>
+#include <iostream>
+#include <conio.h>
+#include "glbisosprog.h"
+
 using namespace std;
 
 int main()
 {
-	cout << "[ISOS QUEMILOS] \n"
-		<< "\n"
-		<< "1.PS5 Simulator\n"
-		<< "2.Blade & Sorcery\n";
-}
+    cout << "---[ISOS QUEMILOS]---\n"
+        << "\n"
+        << "   1. Calculator\n"
+        << "   2. Read File\n"
+        << "   3. Write to File\n"
+        << "   4. Start Menu\n"
+        << "   5. Settings \n"
+        << "   6. Terminal\n"
+        << "----------------------\n";
 
+    char ch = _getch();
+    switch (ch) {
+    case '1':
+        calculator();
+        break;
+    case '2':
+        readFile();
+        break;
+    case '3':
+        writeFile();
+        break;
+    case '4':
+//start menu daha eklenmedi
+        break;
+    case '5':
+        settings();
+//ayarlar daha eklenmedi
+        break;
+    case '6':
+        terminal();
+        break;
+    default:
+        cout << "Invalid option" << endl;
+        break;
+    }
+
+    return 0;
+}
