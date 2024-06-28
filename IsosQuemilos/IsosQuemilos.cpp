a2ade43d1b7488ae29053b1545568670585558f9
Mustafa Kavak <101901319+mustafa0kavak@users.noreply.github.com>
2024-06-28 20:45:51 +0300

    IsosQuemilos.cpp

--- IsosQuemilos/IsosQuemilos.cpp
+++ IsosQuemilos/IsosQuemilos.cpp
@@ -4,42 +4,50 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "---[ISOS QUEMILOS]---\n"
-        << "\n"
-        << "   1. Calculator\n"
-        << "   2. Read File\n"
-        << "   3. Write to File\n"
-        << "   4. Start Menu\n"
-        << "   5. Settings \n"
-        << "   6. Terminal\n"
-        << "----------------------\n";
+int main() {
+    while (true) {
+        cout << "---[ISOS QUEMILOS]---\n"
+            << "\n"
+            << "   1. Calculator\n"
+            << "   2. Read File\n"
+            << "   3. Write to File\n"
+            << "   4. Start Menu\n"
+            << "   5. Terminal\n"
+            << "----------------------\n";
 
-    char ch = _getch();
-    switch (ch) {
-    case '1':
-        calculator();
-        break;
-    case '2':
-        readFile();
-        break;
-    case '3':
-        writeFile();
-        break;
-    case '4':
-//start menu daha eklenmedi
-        break;
-    case '5':
-        settings();
-//ayarlar daha eklenmedi
-        break;
-    case '6':
-        terminal();
-        break;
-    default:
-        cout << "Invalid option" << endl;
-        break;
+        char ch = _getch();
+        switch (ch) {
+        case '1':
+            calculator();
+            break;
+        case '2':
+            readFile();
+            break;
+        case '3':
+            writeFile();
+            break;
+        case '4':
+            cout << "---[Start Menu]---\n"
+                << "|  1. Shutdown   |\n"
+                << "------------------\n";
+            ch = _getch(); // Kullanıcıdan tekrar giriş almak için
+            switch (ch) {
+            case '1':
+                return 0; // Programdan çıkış
+            default:
+                cout << "Invalid option" << endl;
+                system("pause");
+                break;
+            }
+            break;
+        case '5':
+            terminal();
+            break;
+        default:
+            cout << "Invalid option" << endl;
+            system("pause");
+            break;
+        }
     }
 
     return 0;
